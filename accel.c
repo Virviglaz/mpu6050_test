4a104581a71f5db27ce6b385f7f50c7e8f067627
Pavel Nadein <pavelnadein@gmail.com>
2019-12-14 22:20:55 +0000

    new driver used

--- accel.c
+++ accel.c
@@ -7,58 +7,58 @@
 static struct i2c_dev i2c;
 static uint8_t i2c_err = 0;
 
-static uint8_t i2c_wr(uint8_t I2C_Adrs, uint8_t Reg, uint8_t Value)
+static uint8_t i2c_wr(uint8_t Reg, uint8_t Value)
 {
 	i2c_err |= i2c.wr_reg(NULL, 0x68, Reg, &Value, 1);
 	return 0;
 }
 
-static uint8_t i2c_rd(uint8_t I2C_Adrs, uint8_t Reg, uint8_t * buf, uint16_t size)
+static uint8_t i2c_rd(uint8_t Reg, uint8_t * buf, uint16_t size)
 {
 	i2c_err |= i2c.rd_reg(NULL, 0x68, Reg, buf, size);
 	return 0;
 }
 
-static void delay_func(unsigned int ms)
+static void delay_func(void)
 {
 	sleep(1);
 }
 
-static MPU6050_ResultTypeDef result;
-static MPU6050_StructTypeDef accel = {
-	.MPU6050_Result = &result,
-	.WriteReg = i2c_wr,
-	.ReadReg = i2c_rd,
-	.delay_func = delay_func,
-	.GyroSampleRateHz = 10,
-	.FilterOrder = 6,
-	.GyroScale = GYRO_0250d_s,
-	.AccelScale = Scale_2g,
-	.CheckRDY_pin = NULL,
-};
+static struct mpu_real_values acc_data;
 
+static struct mpu_conf accel = {
+	.write_reg = i2c_wr,
+	.read_reg = i2c_rd,
+	.sample_rate_hz = 50,
+	.filter_order = 1,
+	.gyro_scale = GYRO_0250DS,
+	.acc_scale = SCALE_2G,
+	.check_ready_pin = NULL,
+	.zero_point = NULL,
+	.real_values = &acc_data,
+};
 
 int main(void)
 {
-	uint8_t res;
-	float x,y,z;
-	int ret = i2c_init(&i2c, NULL);
-	if (ret) {
+	uint8_t ret;
+	if (ret = i2c_init(&i2c, NULL)) {
 		printf("Error %d\n", ret);
 		return ret;
 	}
+	printf("Struct size: %u\n", sizeof(struct mpu_measdata));
 
-	MPU6050_Init(&accel);
+	mpu6050_init(&accel, delay_func);
 	printf("i2c result: %u\n", i2c_err);
 	
 	while(1) {
-		res = MPU6050_GetResult(&accel);
-		if (!res) {
-			x = (float)accel.MPU6050_Result->X / 16384.0;
-			y = (float)accel.MPU6050_Result->Y / 16384.0;
-			z = (float)accel.MPU6050_Result->Z / 16384.0;
-			printf("x = %.3f, y = %.3f, z = %.3f s = %.3f\t\t\n",
-				x,y,z, sqrtf(x*x+y*y+z*z));
+		if (!mpu6050_get_result(&accel)) {
+			float sum = acc_data.x * acc_data.x;
+			sum += acc_data.y * acc_data.y;
+			sum += acc_data.z * acc_data.z;
+			sum = sqrt(sum);
+
+			printf("x = %.3f, y = %.3f, z = %.3f s = %.3f, t = %.2f\n",
+				acc_data.x, acc_data.y, acc_data.z, sum, acc_data.temp);
 		}
 	}
 	
